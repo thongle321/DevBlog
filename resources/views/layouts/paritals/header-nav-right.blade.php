a6f57fda9dc22f43ce45a858f1c93ade96ae7aee
thongle321 <thongle857@gmail.com>
2025-01-28 19:00:48 +0700

    DevBlog project

---
+++ resources/views/layouts/paritals/header-nav-right.blade.php
@@ -0,0 +1,38 @@
+<div class="mr-20">
+    <ul class="flex space-x-4">
+        <li>
+            <a class="flex space-x-2 items-center hover:text-yellow-900 text-lg text-yellow-500"
+                href="http://127.0.0.1:8000/dashboard">
+                Bài viết
+            </a>
+        </li>
+
+        <li>
+            <a class="flex space-x-2 items-center hover:text-yellow-500 text-lg text-gray-500"
+                href="http://127.0.0.1:8000/blog">
+                Nhãn
+            </a>
+        </li>
+
+        <li>
+            <a class="flex space-x-2 items-center hover:text-yellow-500 text-lg text-gray-500"
+                href="http://127.0.0.1:8000/blog">
+                Danh mục
+            </a>
+        </li>
+
+        <li>
+            <a class="flex space-x-2 items-center hover:text-yellow-500 text-lg text-gray-500"
+                href="http://127.0.0.1:8000/blog">
+                Về tôi
+            </a>
+        </li>
+
+        <li>
+            <a class="flex space-x-2 items-center hover:text-yellow-500 text-lg text-gray-500"
+                href="http://127.0.0.1:8000/blog">
+                Chính sách
+            </a>
+        </li>
+    </ul>
+</div>
